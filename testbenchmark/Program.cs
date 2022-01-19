b387d1268c11a35913f0187d0f5a7d347de2f2f9
Jason Curl <jcurl@arcor.de>
2022-01-19 21:32:22 +0100

    SPrintF: Move performance tests to BenchmarkDotNet
    The BenchmarkDotNet is a better micro-benchmark tool to use to measure performance, than trying to implement it ourselves.
    
    The results of the benchmark tests (.NET 4.8 and .NET Core 3.1) are provided for reference.
    
    Issue: DOTNET-492

---
+++ testbenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RJCP.Core
+{
+    using BenchmarkDotNet.Running;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
---
+++ testbenchmark/Text/SPrintFBenchmark.cs
@@ -0,0 +1,57 @@
+namespace RJCP.Core.Text
+{
+    using System;
+    using BenchmarkDotNet.Attributes;
+    using static StringUtilities;
+
+    public class SPrintFBenchmark
+    {
+        [Benchmark]
+        public void FormatE_Double() => SPrintF("%e", 123456.789);
+
+        [Benchmark]
+        public void FormatE_Double_System() => _ = string.Format("{0:E}", 123456.789);
+
+        [Benchmark]
+        public void FormatE_Single() => SPrintF("%e", 123456.789f);
+
+        [Benchmark]
+        public void FormatE_Single_System() => _ = string.Format("{0:E}", 123456.789f);
+
+        [Benchmark]
+        public void FormatF_Double() => SPrintF("%f", 123456.789);
+
+        [Benchmark]
+        public void FormatF_Double_System() => _ = string.Format("{0:F}", 123456.789);
+
+        [Benchmark]
+        public void FormatF_Single() => SPrintF("%f", 123456.789f);
+
+        [Benchmark]
+        public void FormatF_Single_System() => _ = string.Format("{0:F}", 123456.789f);
+
+        [Benchmark]
+        public void FormatG_Double() => SPrintF("%g", 123456.789);
+
+        [Benchmark]
+        public void FormatG_Double_System() => _ = string.Format("{0:G}", 123456.789);
+
+        [Benchmark]
+        public void FormatG_Single() => SPrintF("%g", 123456.789f);
+
+        [Benchmark]
+        public void FormatG_Single_System() => _ = string.Format("{0:G}", 123456.789f);
+
+        [Benchmark]
+        public void FormatD_Integer() => SPrintF("%d", 16384);
+
+        [Benchmark]
+        public void FormatD_Integer_System() => _ = string.Format("{0}", 16384);
+
+        [Benchmark]
+        public void FormatU_Unsigned() => SPrintF("%u", 16384);
+
+        [Benchmark]
+        public void FormatU_Unsigned_System() => _ = string.Format("{0}", 16384);
+    }
+}
